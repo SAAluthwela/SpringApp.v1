674f445508a371e6fa0f3779eea0bd9f9d1ad579
SAAluthwela <supunakalanka.git@gmail.com>
2022-07-15 02:53:55 +0530

    model tier added

---
+++ src/main/java/com/production/firstapp/model/Employee.java
@@ -0,0 +1,30 @@
+package com.production.firstapp.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity(name = "EMPLOYEE")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false,updatable = false)
+    private Long id;
+    private String name;
+    private  String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false,updatable = false)
+    private String employeeCode;
+
+}
